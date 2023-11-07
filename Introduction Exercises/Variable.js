808fedd551d61c77ecdbd66bd7d83de18e7b489b
BrianDev03 <2102457@utrivieramaya.edu.mx>
2023-11-06 21:28:39 -0500

    first commit

---
+++ Introduction Exercises/Variable.js
@@ -0,0 +1,17 @@
+// Los comentarios pueden hacer que el codigo sea legible //
+
+// Este es mi primer codigo js //
+
+/*
+Los comentarios pueden hacer que el código sea legible, 
+reutilizable e informativo
+fácil de
+*/
+
+
+let cadena = "Hola";
+let booleano = true;
+let indefinido = undefined;
+let nulo = null;
+
+
---
+++ Introduction Exercises/datatypes.js
@@ -0,0 +1,32 @@
+ 
+ // Crear archivo datatypes.js y usar el operador typeof para verificar tipos de datos.  
+console.log(typeof 'Hola');
+console.log(typeof 20);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+// Declarar cuatro variables sin asignar valores.
+let var1, var2, var3, var4;
+
+// Declarar cuatro variables con valores asignados.
+let num = 42;
+let texto = "Hola, mundo";
+let esVerdadero = true;
+let nula = null;
+
+// Declarar variables en varias líneas.
+let nombre = "Brian";
+let apellido = "Zetina";
+let estadoCivil = "Soltero";
+let pais = "Mexico";
+let edad = 20;
+
+// Declarar variables en una línea.
+let nombre2 = "Mi novia", apellido2 = "Si existe", estadoCivil2 = "Casada", pais2 = "Canadá", edad2 = 20;
+
+// Declarar variables myAge y yourAge, asignar valores e imprimir en la consola.
+let myAge = 20;
+let yourAge = 20;
+console.log("Mi edad:", myAge, "| Tu edad:", yourAge);
+
